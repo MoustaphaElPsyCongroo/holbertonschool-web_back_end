352ace535ec7b75752b89a7657ade1d1676a6299
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-09-19 00:14:47 +0200

    11 - Add docstring

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -18,6 +18,7 @@ T = TypeVar('T')
 def safely_get_value(dct: Mapping,
                      key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
+    """Get the key of a dict"""
     if key in dct:
         return dct[key]
     else:
