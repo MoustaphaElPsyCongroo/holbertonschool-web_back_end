a7d896b056ac04caa4490e6364c2dfa6abfaf027
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-09-26 22:20:08 +0200

    2 - Run time for four parallel comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,7 +5,6 @@ called async_comprehension that takes no arguments.
 The coroutine will collect 10 random numbers using an async comprehensing over
 async_generator, then return the 10 random numbers."""
 from typing import List
-import random
 import asyncio
 
 async_generator = __import__('0-async_generator').async_generator
