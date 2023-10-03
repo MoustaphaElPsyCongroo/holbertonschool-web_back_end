c2dd1b11552a671ac559c4c75cb35de3e786c1f1
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-10-03 17:04:23 +0200

    5 - Encrypt passwords using bcrypt

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""User passwords should NEVER be stored in plain text in a database.
+
+Implement a hash_password function that expects one string argument name
+password and returns a salted, hashed password, which is a byte string.
+
+Use the bcrypt package to perform the hashing (with hashpw).
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Encrypts a password"""
+    encoded = password.encode()
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+    return hashed
