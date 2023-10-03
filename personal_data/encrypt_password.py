fcf4de3e6216c743d0439198319afe8ba8270eec
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-10-03 17:07:10 +0200

    6 - Check that a provided password is valid

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
-"""User passwords should NEVER be stored in plain text in a database.
+"""1-
+User passwords should NEVER be stored in plain text in a database.
 
 Implement a hash_password function that expects one string argument name
 password and returns a salted, hashed password, which is a byte string.
 
 Use the bcrypt package to perform the hashing (with hashpw).
+
+2-
+Implement an is_valid function that expects 2 arguments and returns a boolean.
+
+Arguments:
+
+hashed_password: bytes type
+password: string type
+Use bcrypt to validate that the provided password matches the hashed password.
 """
 import bcrypt
 
@@ -14,3 +24,9 @@ def hash_password(password: str) -> bytes:
     encoded = password.encode()
     hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str):
+    """Validates that the provided password matches the hashed password"""
+    encoded_password = password.encode()
+    return bcrypt.checkpw(encoded_password, hashed_password)
