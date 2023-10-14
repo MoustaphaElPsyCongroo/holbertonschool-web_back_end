c6ae8d43336208b2508ffc6e65ac90ef5c7ceb01
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-10-14 17:38:24 +0200

    12 - Find user by session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,6 +4,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from uuid import uuid4
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -67,3 +68,12 @@ class Auth:
         self._db.update_user(user_id, session_id=uuid)
 
         return uuid
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Gets a user from a session id"""
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+        return user
