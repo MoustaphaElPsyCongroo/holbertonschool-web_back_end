7e35bb794dccae4e8f3f52e223d83cc1fe37619e
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-10-19 14:27:38 +0200

    1 - Parametrize a unit test for exceptions with message

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -15,3 +15,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError, "'a'"),
+        ({"a": 1}, ("a", "b"), KeyError, "'b'")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, exception, message):
+        """Test access_nested_map exceptions"""
+        with self.assertRaises(exception) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(message, str(cm.exception))
