78f4babbb10bb7f761441517221e2be2e1dc64c1
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-10-20 11:27:37 +0200

    7 - Fix typo

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -60,7 +60,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
     ])
-    def test_has_licence(self, repo, license_key, expected):
+    def test_has_license(self, repo, license_key, expected):
         """Test has_license"""
         self.assertEqual(GithubOrgClient.has_license(
             repo, license_key), expected)
