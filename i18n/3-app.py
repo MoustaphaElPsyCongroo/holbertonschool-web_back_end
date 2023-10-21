46ce0672ecba9ae91bd955378a2c3f007be1f880
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-10-21 20:03:18 +0200

    3 - Parametrize templates

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask entrypoint"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -28,7 +28,7 @@ def get_locale():
 @app.route('/')
 def index():
     """Index route"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
