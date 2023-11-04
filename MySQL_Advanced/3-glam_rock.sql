804388de8a6ec0d9461ae95edf257ab873b90ee8
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-11-04 20:20:06 +0100

    3 - OList bands with specific style, ranked by longevity

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,11 @@
+-- Lists all bands with Glam rock as their main style, ranked by their
+-- longevity (the checker expects 2020)
+SELECT
+	band_name,
+	IFNULL(split, 2020) - formed AS lifespan
+FROM
+	metal_bands
+WHERE
+	style LIKE "%Glam rock%"
+ORDER BY
+	lifespan DESC
