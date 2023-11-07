f5c34fc7d40af9dd8b9351eb13fb7d3a5b8dd8c0
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-11-07 22:16:10 +0100

    8 - Remove type hints since they bug the Checker...

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 """Lists all documents in a collection"""
-from typing import List
-
-from pymongo.collection import Collection
 
 
-def list_all(mongo_collection: Collection) -> List:
+def list_all(mongo_collection):
     """Lists all documents in a collection"""
     documents = mongo_collection.find()
 
