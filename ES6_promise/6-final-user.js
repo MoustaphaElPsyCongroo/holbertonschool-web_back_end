ca424aab61974cf9a556525d5029892d2ab4e856
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-11-14 00:40:58 +0100

    6 - Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,14 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((responses) => {
     return responses.map((response) => {
       return {
         status: response.status,
-        value: response,
+        value: response.value,
       };
     });
   });
