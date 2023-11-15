64bfc2c002e0c271b2466387a2c5a082678a3afa
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-11-15 22:04:07 +0100

    5 - Partially emulate abstract classes in javascript

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (
+      this.evacuationWarningMessage === undefined &&
+      this.constructor !== Building
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
