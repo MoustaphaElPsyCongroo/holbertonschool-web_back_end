3c0ef980b3a61e5ee50708e62ed13266217ff8f4
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-11-15 22:07:44 +0100

    5 - Fix lint

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,11 +3,11 @@ export default class Building {
     this._sqft = sqft;
 
     if (
-      this.evacuationWarningMessage === undefined &&
-      this.constructor !== Building
+      this.evacuationWarningMessage === undefined
+      && this.constructor !== Building
     ) {
       throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
+        'Class extending Building must override evacuationWarningMessage',
       );
     }
   }
