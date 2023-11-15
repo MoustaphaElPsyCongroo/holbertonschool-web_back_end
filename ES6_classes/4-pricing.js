3592969595cc382c9c06b1ac102c0d86fafae8a0
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-11-15 20:58:16 +0100

    4 - Static methods

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(a) {
+    this._amount = a;
+  }
+
+  set currency(c) {
+    this._currency = c;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
