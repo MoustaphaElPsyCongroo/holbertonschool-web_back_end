37a4d7839b4719409ffaffdb0e7f8284bc268905
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-11-24 18:51:38 +0100

    1 - Using process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+module.exports = (function useProcessStdin() {
+  process.stdin.setEncoding('utf8');
+
+  console.log('Welcome to Holberton School, what is your name?');
+  process.stdin.on('readable', () => {
+    const userInput = process.stdin.read();
+    if (userInput !== null) {
+      process.stdout.write(`Your name is: ${userInput}`);
+    }
+  });
+
+  process.stdin.on('end', () => {
+    console.log('This important software is now closing');
+  });
+}());
