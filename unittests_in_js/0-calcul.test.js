bb7fe234fe5661133e6ec2d38f873784712b9b9c
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-12-03 16:30:04 +0100

    1 - Combining test descriptions

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -14,7 +14,7 @@ describe("calculateNumber", function () {
     assert.equal(calculateNumber(0, 1.7), 2);
   });
 
-  it("should should return the right number", () => {
+  it("should return the sum of arguments", () => {
     assert.equal(calculateNumber(1.3, 0), 1);
     assert.equal(calculateNumber(0, 1.2), 1);
     assert.equal(calculateNumber(1.3, 1.3), 2);
