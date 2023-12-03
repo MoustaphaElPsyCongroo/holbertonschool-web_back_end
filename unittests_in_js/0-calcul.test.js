7ac4cb9124802d88f19fec718db82e759b8cb9ba
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-12-03 16:06:31 +0100

    0 - Add more tests

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,15 +2,24 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", function () {
-  it("should sum two numbers", function () {
-    assert.equal(calculateNumber(1, 2), 3);
+  it("should round the first argument", () => {
+    assert.equal(calculateNumber(1.0, 0), 1);
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(1.7, 0), 2);
   });
 
-  it("should round this sum when positive", function () {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+  it("should round the second argument", () => {
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
   });
 
-  it("should round this sum when negative", function () {
-    assert.equal(calculateNumber(-1.2, -3.7), -5);
+  it("should should return the right number", () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
   });
 });
